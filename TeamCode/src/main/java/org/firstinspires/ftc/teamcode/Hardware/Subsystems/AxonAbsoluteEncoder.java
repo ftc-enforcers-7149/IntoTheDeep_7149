f576c7a033f234ebccda735ec427c5639715ce15
Stefan Dragos <stefand@ehtpal.org>
2025-01-26 21:35:13 -0500

    Wraparound Encoder for Pitch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/AxonAbsoluteEncoder.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Hardware.Subsystems;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.ActionUtils.ActionStructure.PeriodicAction;
+
+public class AxonAbsoluteEncoder implements PeriodicAction {
+
+    private AnalogInput analogInput;
+    private double absolutePosition;
+    private double currentPosition, lastPosition;
+    private double offset;
+    private final double CONVERT_DEG = 3.3/360.0;
+
+    public AxonAbsoluteEncoder(HardwareMap hMap, String name) {
+        analogInput = hMap.get(AnalogInput.class, name);
+        currentPosition = analogInput.getVoltage() * CONVERT_DEG;
+        lastPosition = currentPosition;
+        absolutePosition = currentPosition;
+
+        offset = 0;
+    }
+
+
+    @Override
+    public void periodic() {
+
+        currentPosition = analogInput.getVoltage() * CONVERT_DEG;
+
+        //wrap around from 0 -> 360 (go backward)
+        if (currentPosition > 300 && lastPosition < 60) {
+            absolutePosition += -(lastPosition + (360 - currentPosition));
+
+            //wrap around from 360 -> 0
+        } else if (currentPosition < 0 && lastPosition > 300) {
+            absolutePosition += (360 - lastPosition) + currentPosition;
+        } else {
+            absolutePosition += currentPosition - lastPosition;
+        }
+
+    }
+
+    public double getAbsolutePosition() {
+        return absolutePosition - offset;
+    }
+
+    /**
+     * Sets the current position to the input offset value. Future calls to getAbsolutePosition()
+     * will reflect the new offset.
+     * @param offset
+     */
+    public void setOffset(double offset) {
+        this.offset = offset;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/QuadServoPitch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/QuadServoPitch.java
@@ -1,17 +1,24 @@
 package org.firstinspires.ftc.teamcode.Hardware.Subsystems;
 
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class QuadServoPitch {
+import org.firstinspires.ftc.teamcode.ActionUtils.ActionStructure.PeriodicAction;
+
+public class QuadServoPitch implements PeriodicAction {
 
     //as viewed from the front of the robot
     private CRServo leftServoBottom, leftServoTop, rightServoBottom, rightServoTop;
-    private AnalogInput pitchPositionInput;
+    private AxonAbsoluteEncoder pitchEncoder;
+    private PIDFController controller;
 
-    private double pitchPos;
+    private double pitchPos, target, pitchAngle;
+    private final double POS_PITCH_CONVERSION = 1/3.0;
+
+    private final double ff = 0.03;
 
     public QuadServoPitch(OpMode opMode) {
         leftServoBottom = opMode.hardwareMap.get(CRServo.class, "leftBottom");
@@ -19,9 +26,11 @@ public class QuadServoPitch {
         rightServoBottom = opMode.hardwareMap.get(CRServo.class, "rightBottom");
         rightServoTop = opMode.hardwareMap.get(CRServo.class, "rightTop");
 
-        pitchPositionInput = opMode.hardwareMap.get(AnalogInput.class, "pitchInput");
+        controller = new PIDFController(0,0,0,0);
 
-        pitchPos = 0;
+        pitchEncoder = new AxonAbsoluteEncoder(opMode.hardwareMap, "pitchInput");
+
+        pitchPos = pitchEncoder.getAbsolutePosition();
     }
 
     public void setPower(double power) {
@@ -32,10 +41,29 @@ public class QuadServoPitch {
     }
 
     /**
-     * @return The position of the pitch in degrees (0, 360)
+     * @return The position of the pitch from wraparound encoder
      */
     public double getPosition() {
-        pitchPos = pitchPositionInput.getVoltage() / 3.3 * 360;
         return pitchPos;
+    }
+
+    public double getPitchAngle() {
+        return pitchAngle;
+    }
+
+    @Override
+    public void periodic() {
+
+        //update the encoder inside this periodic call
+        pitchEncoder.periodic();
+
+        //get position from encoder and convert to an angle measure
+        pitchPos = pitchEncoder.getAbsolutePosition();
+        pitchAngle = pitchPos * POS_PITCH_CONVERSION;
+
+        //adds angle based feedforward
+        double pitchPow = controller.calculate(pitchPos, target) + (Math.cos(Math.toRadians(90 - pitchAngle)) * ff);
+
+        setPower(pitchPow);
     }
 }
