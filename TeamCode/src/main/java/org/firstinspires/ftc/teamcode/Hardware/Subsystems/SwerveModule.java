ae2e1d9b718e5a30b480ec9fb3635f89abd3a277
Shantanu Das <281640@mainlandregional.net>
2025-06-21 13:50:40 -0400

    Swerve Chassis Program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/SwerveModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/SwerveModule.java
@@ -1,20 +1,41 @@
 package org.firstinspires.ftc.teamcode.Hardware.Subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveModuleState;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Config
 public class SwerveModule {
     public DcMotor driveMotor;
-    public Servo pivotServo;
+    public CRServo pivotServo;
+    public AnalogInput axonEncoder;
 
-    public SwerveModule(HardwareMap hardwareMap, String motorName, String servoName) {
+    public PIDFController crController;
+    public static double kP, kD;
+
+    public static final double VOLTAGE_TO_ANGLE = 360 / 3.3;
+
+    public SwerveModule(HardwareMap hardwareMap, String motorName, String servoName, String encoderName) {
         driveMotor = hardwareMap.get(DcMotor.class, motorName);
-        pivotServo = hardwareMap.get(Servo.class, servoName);
+        pivotServo = hardwareMap.get(CRServo.class, servoName);
+        axonEncoder = hardwareMap.get(AnalogInput.class, encoderName);
+
+        crController = new PIDFController(kP, 0, kD, 0);
     }
 
     public void setModuleState(double drivePower, double pivotPosition) {
         driveMotor.setPower(drivePower);
-        pivotServo.setPosition(pivotPosition);
+
+        double power = crController.calculate(axonEncoder.getVoltage() * VOLTAGE_TO_ANGLE, pivotPosition);
+        pivotServo.setPower(power);
+    }
+
+    public void setModuleState(SwerveModuleState moduleState) {
+        setModuleState(moduleState.speedMetersPerSecond, moduleState.angle.getDegrees());
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewSeasonCode/ShanSwerveAttempt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewSeasonCode/ShanSwerveAttempt.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.NewSeasonCode;
 
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveModuleState;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,10 +18,12 @@ public class ShanSwerveAttempt extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        SwerveModule fl = new SwerveModule(hardwareMap, "flWheel", "flPivot");
-        SwerveModule fr = new SwerveModule(hardwareMap, "frWheel", "frPivot");
-        SwerveModule bl = new SwerveModule(hardwareMap, "blWheel", "blPivot");
-        SwerveModule br = new SwerveModule(hardwareMap, "brWheel", "brPivot");
+        SwerveModule fl = new SwerveModule(hardwareMap, "flWheel", "flPivot", "flEncoder");
+        SwerveModule fr = new SwerveModule(hardwareMap, "frWheel", "frPivot", "frEncoder");
+        SwerveModule bl = new SwerveModule(hardwareMap, "blWheel", "blPivot", "blEncoder");
+        SwerveModule br = new SwerveModule(hardwareMap, "brWheel", "brPivot", "brEncoder");
+
+        SwerveModule[] swerveModules = {fl, fr, bl, br};
 
         Translation2d m_frontLeftLocation =
                 new Translation2d(0.381, 0.381);
@@ -30,5 +34,32 @@ public class ShanSwerveAttempt extends LinearOpMode {
         Translation2d m_backRightLocation =
                 new Translation2d(-0.381, -0.381);
 
+        // Creating my kinematics object using the module locations
+        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+                  m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+        );
+
+        waitForStart();
+
+        if (isStopRequested()) {
+            return;
+        }
+
+        while (opModeIsActive()) {
+
+            double forward = -gamepad1.left_stick_y;
+            double strafe  = gamepad1.left_stick_x;
+            double rotation = gamepad1.right_stick_x;
+
+            ChassisSpeeds speeds = new ChassisSpeeds(forward, strafe, rotation);
+
+            SwerveModuleState[] moduleStates =
+                    m_kinematics.toSwerveModuleStates(speeds);
+
+            for (int i = 0; i < 4; i++) {
+                swerveModules[i].setModuleState(moduleStates[i]);
+            }
+
+        }
     }
 }
